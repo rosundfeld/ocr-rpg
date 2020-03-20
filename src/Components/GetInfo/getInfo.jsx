6fcfc14addc0be865935f746463f629bed427314
Rodrigo Sundfeld <rodrigo.psundfeld@gmail.com>
2020-03-19 22:22:56 -0300

    socorro

--- src/Components/GetInfo/getInfo.jsx
+++ src/Components/GetInfo/getInfo.jsx
@@ -4,6 +4,22 @@ import { useEffect } from 'react';
 export default function GetInfo() {
 
   const [imagePreview, setImagePreview] = useState(null);
+  const [imageData, setImageData] = useState(null);
+
+  useEffect(async () => {
+    const config = {
+      method: "GET",
+      headers: {
+        Accept: "app/json",
+        "Content-Type": "app/json",
+        "Access-Control-Allow-Credentials": "true"
+      },
+      mode: "cors"
+    };
+    const resp = await fetch('http://127.0.0.1:5000/ocr-route',
+    config);
+    console.log(resp)
+  }, []);
 
   function handleImage(event){
     setImagePreview({
