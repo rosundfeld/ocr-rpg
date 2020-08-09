386fdb5c4ac7624579ca6cd461b671b22aa90023
Rodrigo Sundfeld <rodrigo.psundfeld@gmail.com>
2020-08-09 19:02:10 -0300

    Campanhas 50%

--- src/Views/Campanhas/Campanhas.jsx
+++ src/Views/Campanhas/Campanhas.jsx
@@ -1,24 +1,67 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 
+// Material UI
 import { Paper, Avatar } from "@material-ui/core";
+
+//MakeStyles
 import { makeStyles } from "@material-ui/core/styles";
 
+//Components
+import CampaingCard from '../../Components/CampaingCard/campaignCard'
+
+//Wallpaper
+import forestBG from '../../Assets/CampWallpaper/campWallpaper.jpg'
+
+
 const useStyles = makeStyles({
-  navIcon: {
-    "&:hover": {
-      cursor: "pointer",
-      backgroundColor: "#944b44",
-    },
+  bookContainer: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    margin: "2rem",
   },
+  container: {
+    height: "100%",
+    background:  `url(${forestBG}) no-repeat center center fixed`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    position: "relative",
+    backgroundAttachment: "fixed",
+    backgroundColor: "black"
+  }
 });
 
 export default function Campanhas() {
   const classes = useStyles();
 
-  return <div>
-    <div>
-      <h1 style={{ margin: "auto", marginTop: "20%" }}>Campanhas</h1>
+  const campaignSettings = [
+    {
+      campaignName:"Gods Name", 
+      character:"Miffhir", 
+      master:"Pedro Pinho"
+    },
+    {
+      campaignName:"Aventura Fuinhal", 
+      character:"Bixu", 
+      master:"Sophia Schuster"
+    },
+    {
+      campaignName:"Valorozo", 
+      character:"Phoenix", 
+      master:"Riot Games"
+    },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.bookContainer}>
+        {campaignSettings.map((camp) =>
+          <CampaingCard camp={camp} />
+          )}
+          <CampaingCard camp={null}/>
+      </div>
     </div>
-  </div>;
+  );
 }
